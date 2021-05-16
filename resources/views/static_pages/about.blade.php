f0e645eea8ef79691daf564f1e4a9cd0b0dce764
Aring <tantubuping@163.com>
2021-05-17 00:57:44 +0800

    day1

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html> <head>
+    <title>Weibo App</title>
+</head> <body>
+<h1>about</h1>
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html> <head>
+    <title>Weibo App</title>
+</head> <body>
+<h1>主页</h1>
+</body>
+</html>
